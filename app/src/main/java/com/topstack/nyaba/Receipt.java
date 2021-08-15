21a560e46d547c9e18d874c87687dede8b450c89
dona Mondal <donamondal25@gmail.com>
2021-08-15 11:08:59 +0530

    Receipt layout done

---
+++ app/src/main/java/com/topstack/nyaba/Receipt.java
@@ -0,0 +1,14 @@
+package com.topstack.nyaba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Receipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt);
+    }
+}
